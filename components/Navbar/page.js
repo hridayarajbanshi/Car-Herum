ee480c77d2d82e44281fd05d4351a1a6cc876ab2
Hanny <35860420+hridayarajbanshi@users.noreply.github.com>
2024-01-10 10:13:47 +0545

    added pages

--- components/Navbar/page.js
+++ components/Navbar/page.js
@@ -1,8 +1,18 @@
 // components/Navbar.js
+'use client';
 import Link from 'next/link';
 import { IoIosBug } from "react-icons/io";
 import react from 'react';
+import { usePathname } from 'next/navigation';
 const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+  const links = [
+    {label: 'Dashboard', href: '/'},
+    {label: 'Issues', href: '/issues'},
+    
+    {label: 'About', href: '/about'},
+  ]
   return (
     <nav className="bg-black p-4">
       <div className="container mx-auto flex items-center justify-between ">
@@ -10,9 +20,13 @@ const Navbar = () => {
           <IoIosBug className="text-aquamarine text-4xl" />
         </Link>
         <ul className='flex items-center gap-12'>
-        <li className='text-white hover:text-xl transition-all'><Link href="/">Dashboard</Link></li>
-        <li className='text-white hover:text-xl transition-all'><Link href="/">Issues</Link></li>
-          <li className='text-white hover:text-xl transition-all'><Link href="/">About</Link></li>
+        {links.map(link =>
+          <Link key={link.href}
+          href={link.href}
+          className={`${link.href === currentPath ? 'text-white': 'text-gray-400'}  hover:text-aquamarine`}
+          >{link.label}</Link>
+          )}
+
         </ul>
         <div className="hidden md:flex space-x-4">
         
